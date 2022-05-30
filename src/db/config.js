4f9a314eb7aab0fba745058afa0db2901fedc0b3
ifCoder <ifcoderweb@gmail.com>
2022-05-29 22:48:44 -0300

    ajuste seo e var

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://ifcoderweb:92558164@cluster0.lcvh8.mongodb.net/mlmonitor?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: process.env.NODE_ENV}
 }else{
     module.exports = {mongoURI: "mongodb://localhost:27017/mlsistem"}
 }
