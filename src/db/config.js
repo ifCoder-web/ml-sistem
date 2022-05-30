04f1ff25dd8179a8041ced0d07a22410c8403690
ifCoder <ifcoderweb@gmail.com>
2022-05-29 22:51:07 -0300

    ajuste var ambiente

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: process.env.NODE_ENV}
+    module.exports = {mongoURI: process.env.MONGO_URI}
 }else{
     module.exports = {mongoURI: "mongodb://localhost:27017/mlsistem"}
 }
